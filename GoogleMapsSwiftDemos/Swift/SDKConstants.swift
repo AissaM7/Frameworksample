86335f6dff9326f21b0847da6d8eb6514f7a3a49
Aissa <Aiss>
2023-10-30 15:11:41 -0400

    GoogleMapsBase Framework Sample App

--- GoogleMapsSwiftDemos/Swift/SDKConstants.swift
+++ GoogleMapsSwiftDemos/Swift/SDKConstants.swift
@@ -21,7 +21,7 @@ enum SDKConstants {
 
 extension CLLocationCoordinate2D {
   static let sydney = CLLocationCoordinate2D(latitude: -33.8683, longitude: 151.2086)
-  // Victoria, Australia
+ 
   static let victoria = CLLocationCoordinate2D(latitude: -37.81969, longitude: 144.966085)
   static let newYork = CLLocationCoordinate2D(latitude: 40.761388, longitude: -73.978133)
   static let mountainSceneLocation = CLLocationCoordinate2D(
